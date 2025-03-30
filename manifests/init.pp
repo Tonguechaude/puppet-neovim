6ca52d9c6c73a5202f386a00abf8f68c1284e930
tongue <evan.challias@tonguechaude.fr>
2025-03-31 00:08:45 +0200

    feat : v1 du module

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,9 @@
 #
 # A description of what this class does
 #
+# @param user User account name
+# @param group User account group name
+#
 # @example
 #   include neovim
 class neovim (
@@ -14,7 +17,7 @@ class neovim (
   -> package { 'vim':
     ensure => absent,
   }
-  -> file { ['/usr/bin/vi', '/usr/bin/vim.tiny', '/usr/bin/vimtutor']:
+  -> file { ['/usr/bin/vi', '/usr/bin/vim.tiny', '/usr/bin/vimtutor', "/home/${user}/.vim"]:
     ensure => absent,
   }
   -> file { "/home/${user}/.config":
@@ -23,7 +26,13 @@ class neovim (
     group  => $group,
     mode   => '0755',
   }
-  -> vcsrepo { "/home/${user}/.config/neovim":
+  file { '/opt':
+    ensure => directory,
+    owner  => $user,
+    group  => $group,
+    mode   => '0755',
+  }
+  -> vcsrepo { '/opt/neovim':
     ensure   => present,
     provider => git,
     source   => 'https://github.com/neovim/neovim.git',
@@ -33,26 +42,19 @@ class neovim (
   }
   -> exec { 'compile_neovim':
     command => 'make CMAKE_BUILD_TYPE=RelWithDebInfo',
-    cwd     => "/home/${user}/.config/neovim",
+    cwd     => '/opt/neovim',
     user    => $user,
     path    => '/usr/sbin:/usr/bin:/sbin:/bin',
-    require => Vcsrepo["/home/${user}/.config/neovim"],
+    require => Vcsrepo['/opt/neovim'],
   }
   -> exec { 'install_neovim':
     command => 'sudo make install',
-    cwd     => "/home/${user}/.config/neovim",
+    cwd     => '/opt/neovim',
     user    => $user,
-    path    => '/usr/sbin:/usr/bin:/sbin:/bin',
+    path    => '/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin',
     require => Exec['compile_neovim'],
   }
-  -> exec { 'clean workstation':
-    command => 'rm -rf puppet7-release-jammy.deb .config/neovim',
-    cwd     => "/home/${user}/",
-    user    => $user,
-    path    => '/usr/sbin:/usr/bin:/sbin:/bin',
-    require => Exec['compile_neovim', 'install_neovim'],
-  }
-  -> vcsrepo { "/home/${user}/.config/neovim":
+  -> vcsrepo { "/home/${user}/.config/neovimconf":
     ensure   => present,
     provider => git,
     source   => 'https://github.com/Tonguechaude/neovim.git',
@@ -60,8 +62,46 @@ class neovim (
     group    => $group,
     require  => Package['git'],
   }
-  -> file { "/home/${user}/.config/neovim/nvim":
-    ensure => link,
-    target => "/home/${user}/.config/nvim",
+  -> file { "/home/${user}/.config/nvim":
+    ensure  => directory,
+    links   => follow,
+    source  => "/home/${user}/.config/neovimconf/nvim",
+    recurse => true,
+    owner   => $user,
+    group   => $group,
+  }
+  -> exec { 'Install nodeJS for COC':
+    command     => "bash -c \"export HOME=/home/${user} && curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash && source /home/${user}/.nvm/nvm.sh && nvm install 22\"",
+    cwd         => "/home/${user}/",
+    user        => $user,
+    path        => '/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin',
+    environment => ["HOME=/home/${user}"],
+  }
+  -> exec { 'install_vim_plug':
+    command => "curl -fLo /home/${user}/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+              https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim",
+    creates => "/home/${user}/.local/share/nvim/site/autoload/plug.vim",
+    user    => $user,
+    path    => '/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin',
+  }
+  -> exec { 'Install nvim Plugin':
+    command => 'nvim --headless +PlugInstall +qall',
+    cwd     => "/home/${user}/",
+    user    => $user,
+    path    => '/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin',
+  }
+  exec { 'install_coc_dependencies':
+    command => 'npm install',
+    cwd     => "/home/${user}/.config/nvim/bundle/coc.nvim",
+    user    => $user,
+    path    => "/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin:/home/${user}/.nvm/versions/node/v22.14.0/bin",
+    creates => "/home/${user}/.config/nvim/bundle/coc.nvim/build/index.js",
+  }
+  -> exec { 'clean workstation':
+    command => 'rm -rf puppet7-release-jammy.deb /opt/neovim',
+    cwd     => "/home/${user}/",
+    user    => $user,
+    path    => '/usr/sbin:/usr/bin:/sbin:/bin',
+    require => Exec['compile_neovim', 'install_neovim'],
   }
 }
